dae7f81dbfa265a767a0e53c437a62c18844abba
Anatoly Yakovlev <yakovlef@gmail.com>
2018-11-08 15:22:37 +0400

    GeekBrains: Homework - Java 3, lesson 01

---
+++ Apple.java
@@ -0,0 +1,7 @@
+package Java2.lesson1;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super(1f);
+    }
+}
---
+++ Orange.java
@@ -0,0 +1,7 @@
+package Java2.lesson1;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+}
